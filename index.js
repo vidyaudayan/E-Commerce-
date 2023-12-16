4a2a3a96698a52faae8e1d015913108b9a9481c3
vidyaudayan <vidyaudayan89@gmail.com>
2023-12-16 19:52:58 +0530

    Database andproduct model added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 const cors = require('cors')
 const productRoutes = require('./routes/productRoutes')
 const categoryRoutes=require('./routes/categoryRoutes')
@@ -17,3 +19,15 @@ app.use('/api/v1/categories',categoryRoutes)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+main().then(()=>console.log("DB connected")).catch(err => console.log(err));
+
+async function main() {
+  const url= process.env.DB_URL
+  const password= process.env.DB_PASSWORD
+  const urlWithPassword= url.replace('<password>',password)
+  await mongoose.connect(urlWithPassword);
+
+}
